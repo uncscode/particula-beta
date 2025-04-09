99c56141f25add23aa9a0cb014c65c6ca4f439d1
Kyle Gorkowski <kkgorkow@gmail.com>
2025-04-09 15:17:29 -0600

    init update

--- particula_beta/__init__.py
+++ particula_beta/__init__.py
@@ -8,3 +8,9 @@ Particula-beta
 __version__ = "0.0.3"
 
 from particula_beta import data
+from particula_beta.time_manage import (
+    time_str_to_epoch,
+    relative_time,
+    datetime64_from_epoch_array,
+)
+from particula_beta.units import convert_units
